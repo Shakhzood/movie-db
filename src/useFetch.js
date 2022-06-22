ea7dd50a5c774f1e40709c60520005e474d8d54a
Shakhzod Jumaev <shakhzod_jumaev@epam.com>
2022-06-22 15:24:23 +0500

    useFetch custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+
+const useFetch = (urlParams) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [data, setData] = useState([]);
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.Response === "True") {
+        setData(data.Search || data);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+      //   console.log(data);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}${urlParams}`);
+  }, [urlParams]);
+
+  return { isLoading, error, data };
+};
+
+export default useFetch;
